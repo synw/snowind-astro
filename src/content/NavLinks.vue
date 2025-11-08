32fe4733a085e3f62f082621e4d1e0c2204e3065
synw <synwe@yahoo.com>
2025-11-08 14:01:27 +0100

    Refactor navigation and content structure by removing deprecated Hero component and reorganizing page layouts
    - Removed Hero.astro component and its references from page layouts
    - Simplified navigation links in NavLinks.vue by removing unused link
    - Updated index pages (en and fr) to use siteTitle from configuration instead of hardcoded titles
    - Added style guide link in index pages instead of direct page links
    - Removed page1.astro files that were using the deprecated Hero component

--- src/content/NavLinks.vue
+++ src/content/NavLinks.vue
@@ -1,6 +1,5 @@
 <template>
-    <a class="btn menu-link" :href="lang+'/page1'">Page 1</a>
-    <a class="btn menu-link" href="#">Link2</a>
+    <a class="btn menu-link" :href="lang + '/page1'">Page 1</a>
 </template>
 
 <script setup lang="ts">
